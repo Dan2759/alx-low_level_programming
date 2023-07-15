f9dcf742625d083d05d238beed865ab6d0af733f
Dan2759 <kwizeradan.kd27@gmail.com>
2023-07-14 17:08:52 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,22 +7,19 @@ int main(void)
 {
 int i, j;
 
-for (i = 0; i < 100; i++)
+for (i = 0; i <= 99; i++)
 {
-for (j = 0; j < 100; j++)
+for (j = i; j <= 99; j++)
 {
-if (i < j)
-{
-putchar((i / 10) + 48);
-putchar((i % 10) + 48);
+putchar('0' + (i / 10));
+putchar('0' + (i % 10));
 putchar(' ');
-putchar((j / 10) + 48);
-putchar((j % 10) + 48);
-if (!(i != 98 || j != 99))
+putchar('0' + (j / 10));
+putchar('0' + (j % 10));
+if (i != 99 || j != 99)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
