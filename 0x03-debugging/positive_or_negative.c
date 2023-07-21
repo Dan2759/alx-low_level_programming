3120e95f37d84f9f379482a3d5944a593576beb1
Dan2759 <kwizeradan.kd27@gmail.com>
2023-07-20 17:13:54 -0700

    1-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,22 +1,12 @@
+#include <stdio.h>
 #include "main.h"
-/**
- * main - Entry point
- * Return: 0 (Success)
- */
 
 void positive_or_negative(int i)
 {
-if (i < 0)
-{
-printf("%d is %s\n", i, "negative");
-}
-else if (i > 0)
-{
-printf("%d is %s\n", i, "positive");
-}
-else
-{
-printf("%d is %s\n", i, "zero");
-}
-return;
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero'n", i);
+	else
+		printf("%d is negative\n", i);
 }
